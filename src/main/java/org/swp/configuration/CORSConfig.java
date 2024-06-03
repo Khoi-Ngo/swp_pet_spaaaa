03663458bc61cb9fe2fddac85c68c6bc149b9d6c
Khoi-Ngo <khoingo2003@gmail.com>
2024-06-03 12:17:01 +0700

    updating CORS with remote domain

--- src/main/java/org/swp/configuration/CORSConfig.java
+++ src/main/java/org/swp/configuration/CORSConfig.java
@@ -9,7 +9,7 @@ public class CORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("perfect-education-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
