89a3a288c21d3246e71c41b263d5c64b72f39e04
Khoi-Ngo <khoingo2003@gmail.com>
2024-06-06 20:39:29 +0700

    mapping API: update mapping category dto

--- src/main/java/org/swp/service/CategoryServiceService.java
+++ src/main/java/org/swp/service/CategoryServiceService.java
@@ -1,5 +1,6 @@
 package org.swp.service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.swp.dto.response.ServiceCategoryDto;
@@ -13,11 +14,13 @@ import java.util.List;
 public class CategoryServiceService {
     @Autowired
     private ICategorySerivceRepository categorySerivceRepository;
+    @Autowired
+    private ModelMapper modelMapper;
 
     public List<ServiceCategoryDto> getAll() {
         List<ServiceCategory> serviceCategories = categorySerivceRepository.findAll();
-        List<ServiceCategoryDto> serviceCategoryDtos = new ArrayList<ServiceCategoryDto>();
-        serviceCategories.forEach(sc -> serviceCategoryDtos.add(new ServiceCategoryDto()));
+        List<ServiceCategoryDto> serviceCategoryDtos = new ArrayList<>();
+        serviceCategories.forEach(sc -> serviceCategoryDtos.add(modelMapper.map(sc, ServiceCategoryDto.class)));
         return serviceCategoryDtos;
     }
 }
