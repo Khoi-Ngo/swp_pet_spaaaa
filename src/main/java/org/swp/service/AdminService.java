c8f4f065c4ef9c544d500c8f9b50f6c24494e847
Utsuro1 <thanhthoai90813@gmail.com>
2024-06-23 16:02:09 +0700

    Validation username exist

--- src/main/java/org/swp/service/AdminService.java
+++ src/main/java/org/swp/service/AdminService.java
@@ -62,6 +62,9 @@ public class AdminService {
         if (userRepository.findByEmail(signUpRequest.getEmail()).isPresent()) {
             throw new IllegalArgumentException("Email already exists");
         }
+        if (userRepository.findByUsername(signUpRequest.getUsername()).isPresent()) {
+            throw new IllegalArgumentException("Username already exists");
+        }
         User user = modelMapper.map(signUpRequest, User.class);
         user.setPassword(passwordEncoder.encode(signUpRequest.getPassword()));
         user.setRole(UserRole.SHOP_OWNER);
