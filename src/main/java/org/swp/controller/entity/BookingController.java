e2910b96dac5385f803709c7f7b05ebff35ba320
Khoi-Ngo <khoingo2003@gmail.com>
2024-06-28 01:33:58 +0700

    add missed import 2

--- src/main/java/org/swp/controller/entity/BookingController.java
+++ src/main/java/org/swp/controller/entity/BookingController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.swp.dto.request.RequestBookingRequest;
 import org.swp.dto.request.RequestCancelBookingRequest;
+import org.swp.enums.BookingStatus;
 import org.swp.service.BookingService;
 
 import java.time.LocalDate;
