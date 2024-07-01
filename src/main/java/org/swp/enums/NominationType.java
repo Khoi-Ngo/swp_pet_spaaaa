ce8021e4b3b256b4182d2feb9ea47466450ae325
Khoi-Ngo <khoingo2003@gmail.com>
2024-07-01 09:43:18 +0700

    Revert "add api Nomination (view/create)"
    This reverts commit fb37e0716810780154d9b33f38643fb6e3cf9b4f.

--- src/main/java/org/swp/enums/NominationType.java
+++ src/main/java/org/swp/enums/NominationType.java
@@ -1,27 +1,9 @@
 package org.swp.enums;
 
 public enum NominationType {
-    BAD(0),
-    NORMAL(1),
-    QUITE_GOOD(2),
-    REALLY_GOOD(3);
+    BAD,
+    NORMAL,
+    QUITE_GOOD,
+    REALLY_GOOD
 
-    private final int value;
-
-    NominationType(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public static NominationType fromValue(int value) {
-        for (NominationType type : NominationType.values()) {
-            if (type.getValue() == value) {
-                return type;
-            }
-        }
-        throw new IllegalArgumentException("Invalid nomination type value: " + value);
-    }
 }
--- src/main/java/org/swp/repository/INominationRepository.java
+++ src/main/java/org/swp/repository/INominationRepository.java
@@ -1,19 +1,7 @@
 package org.swp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.swp.entity.other.Nomination;
 
-import java.util.List;
-
 public interface INominationRepository extends JpaRepository<Nomination, Integer> {
-
-    @Query(value = "SELECT * FROM tbl_nomination WHERE service_id = :serviceId ORDER BY id DESC", nativeQuery = true)
-    List<Nomination> findAllByServiceId(@Param("serviceId") int serviceId);
-
-    @Query(value = "SELECT * FROM tbl_nomination WHERE shop_id = :shopId ORDER BY id DESC", nativeQuery = true)
-    List<Nomination> findAllByShopId(@Param("shopId") int shopId);
 }
-
-
