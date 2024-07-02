5bd41b6b4f78bae463ba7513935ecf00388250ea
Khoi-Ngo <khoingo2003@gmail.com>
2024-07-02 15:45:20 +0700

    add refer price table

---
+++ src/main/java/org/swp/repository/IReferPriceRepository.java
@@ -0,0 +1,15 @@
+package org.swp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.swp.entity.ReferPrice;
+
+import java.util.List;
+
+@Repository
+public interface IReferPriceRepository extends JpaRepository<ReferPrice, Integer> {
+    @Query(value = "SELECT * FROM tbl_refer_price WHERE service_id = :serviceId", nativeQuery = true)
+    List<ReferPrice> findByServiceId(@Param("serviceId") int serviceId);
+}
