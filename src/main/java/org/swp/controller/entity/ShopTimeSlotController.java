046d57a64b37b6cd421de03bb595ebe697a68607
Khoi-Ngo <khoingo2003@gmail.com>
2024-07-07 23:48:55 +0700

    remove

--- src/main/java/org/swp/controller/entity/ShopTimeSlotController.java
+++ src/main/java/org/swp/controller/entity/ShopTimeSlotController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.swp.dto.request.CreateShopTimeSlotRequest;
-import org.swp.dto.request.UpdateShopTimeSlotRequest;
 import org.swp.service.ShopTimeSlotService;
 
 
